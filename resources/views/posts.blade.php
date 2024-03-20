1f0cc5faac2808d834f62131de42779c568c4d78
faisal muaris <faisal.muaris.fm@gmail.com>
2024-03-20 10:35:59 +0700

    blade templating engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     @foreach ($posts as $post)
         <article class="mb-5">
-            <h2>{{ $post['title'] }}</h2>
+            <h2>
+                <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+            </h2>
             <h4>by. {{ $post['author'] }}</h4>
             <p>{{ $post['body'] }}</p>
             <hr>
--- routes/web.php
+++ routes/web.php
@@ -39,13 +39,15 @@ Route::get('/blog', function () {
     $blog_posts = [
         [
             'title' => 'Judul Post Pertama',
+            'slug' => 'judul-post-pertama',
             'author' => 'Faisal Muaris',
-            'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum quod placeat facilis fuga? Ullam deleniti ipsam explicabo, sunt repudiandae perferendis animi eveniet, dignissimos sint voluptatibus in quam libero omnis quaerat molestias quis suscipit saepe, mollitia inventore a ad veniam.'
+            'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
         ],
         [
             'title' => 'Judul Post Kedua',
+            'slug' => 'judul-post-kedua',
             'author' => 'Arif Lukmanulhakim',
-            'body' =>  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eligendi quisquam provident totam? Beatae sunt quo veritatis, provident culpa eius temporibus et neque nobis amet dolorem ab incidunt consequuntur architecto eos exercitationem in corporis repellat praesentium assumenda minus. Distinctio pariatur culpa beatae quis deserunt nihil quo laudantium placeat, perferendis, ea vitae iusto incidunt consequatur dolores fugit blanditiis error aliquam tenetur?'
+            'body' =>  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eligendi quisquam provident totam? Beatae sunt quo veritatis, provident culpa eius temporibus et neque nobis amet dolorem ab incidunt '
         ],
     ];
 
@@ -54,3 +56,37 @@ Route::get('/blog', function () {
         'posts' => $blog_posts
     ]);
 });
+
+
+// Halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            'title' => 'Judul Post Pertama',
+            'slug' => 'judul-post-pertama',
+            'author' => 'Faisal Muaris',
+            'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum quod placeat facilis fuga? Ullam deleniti ipsam explicabo, sunt repudiandae perferendis animi eveniet, dignissimos sint voluptatibus in quam libero omnis quaerat molestias quis suscipit saepe, mollitia inventore a ad veniam.'
+        ],
+        [
+            'title' => 'Judul Post Kedua',
+            'slug' => 'judul-post-kedua',
+            'author' => 'Arif Lukmanulhakim',
+            'body' =>  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eligendi quisquam provident totam? Beatae sunt quo veritatis, provident culpa eius temporibus et neque nobis amet dolorem ab incidunt consequuntur architecto eos exercitationem in corporis repellat praesentium assumenda minus. Distinctio pariatur culpa beatae quis deserunt nihil quo laudantium placeat, perferendis, ea vitae iusto incidunt consequatur dolores fugit blanditiis error aliquam tenetur?'
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post ){
+        if ($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' =>$new_post
+    ]);
+});
+
+
+
