ae0f198ce389d7201987f3a368482ecbbc418066
faisal muaris <faisal.muaris.fm@gmail.com>
2024-03-21 08:53:06 +0700

    Post Model & Model Binding(penting)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    // use HasFactory;
+    use HasFactory;
 
-   private static $blog_posts = [
-    [
-        'title' => 'Judul Post Pertama',
-        'slug' => 'judul-post-pertama',
-        'author' => 'Faisal Muaris',
-        'body' =>  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nostrum quod placeat facilis fuga? Ullam deleniti ipsam explicabo, sunt repudiandae perferendis animi eveniet, dignissimos sint voluptatibus in quam libero omnis quaerat molestias quis suscipit saepe, mollitia inventore a ad veniam.'
-    ],
-    [
-        'title' => 'Judul Post Kedua',
-        'slug' => 'judul-post-kedua',
-        'author' => 'Arif Lukmanulhakim',
-        'body' =>  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni eligendi quisquam provident totam? Beatae sunt quo veritatis, provident culpa eius temporibus et neque nobis amet dolorem ab incidunt consequuntur architecto eos exercitationem in corporis repellat praesentium assumenda minus. Distinctio pariatur culpa beatae quis deserunt nihil quo laudantium placeat, perferendis, ea vitae iusto incidunt consequatur dolores fugit blanditiis error aliquam tenetur?'
-    ],
-];
-
-public static function all()
-{
-    return collect(self::$blog_posts);
-}
-
-public static function find($slug)
-{
-    $posts = static::all();
-    return $posts->firstWhere('slug', $slug);
-}
-
+    // protected $guarded = ['id'];
+    protected $fillable = [
+        'title',
+        'excerpt',
+        'body'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class,'index']);
 
 // Halaman single post
-Route::get('posts/{slug}', [PostController::class,'show']);
+Route::get('posts/{post:slug}', [PostController::class,'show']);
 
 
 
